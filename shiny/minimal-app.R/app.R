967cdabd98cfc32f9f3c4a49f2d90525cca98a51
Tillmann Schwörer <till.schwoerer@gmx.de>
2020-04-13 23:42:12 +0200

    Add section files

---
+++ shiny/minimal-app.R/app.R
@@ -0,0 +1,28 @@
+library(shiny)
+
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Old Faithful Geyser Data"),
+
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("bins",
+                        "Number of bins:",
+                        min = 1,
+                        max = 50,
+                        value = 30)
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+            plotOutput("distPlot")
+        )
+    )
+)
+
+server <- function(input, output, sesion) {}
+
+# Run the application
+shinyApp(ui = ui, server = server)
---
+++ shiny/minimal-app/app.R
@@ -0,0 +1,9 @@
+library(shiny)
+
+ui <- fluidPage(
+  titlePanel("sddsdfsdfsdfasdf")
+)
+
+server <- function(input, output, session) {}
+
+shinyApp(ui = ui, server = server)
